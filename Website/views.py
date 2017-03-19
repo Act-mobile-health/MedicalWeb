c55ad7c34542d077ca4c1fee79e8a7c768558cda
yingchun <zhangyc@TestPC>
2017-03-19 18:44:26 +0800

    @0319

--- Website/views.py
+++ Website/views.py
@@ -7,3 +7,24 @@ def test(request):
 
 def index(request):
     return render(request,"index.html")
+
+def addOutpatientInfos(request):
+    return render(request,"outpatientInfos.html")
+
+def addEmergencyInfos(request):
+    return render(request,"emergencyInfos.html")
+
+def addHospitalInfos(request):
+    return render(request,"hospitalInfos.html")
+
+def addPatientInfos(request):
+    return render(request,"patientInfos.html")
+
+def addESS(request):
+    return render(request,"ESS.html")
+
+def addMBQ(request):
+    return render(request,"MBQ.html")
+
+def addClinicInfos(request):
+    return render(request,"clinicInfos.html")
